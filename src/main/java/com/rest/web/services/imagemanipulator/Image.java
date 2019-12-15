e55951639f7845ca7bcca8a377e49d92b88e8d2f
Lukasz Wojcik <lukasz.wojcik.96@gmail.com>
2019-12-15 18:28:55 +0100

    fixed missing entity annotation

--- src/main/java/com/rest/web/services/imagemanipulator/Image.java
+++ src/main/java/com/rest/web/services/imagemanipulator/Image.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import javax.persistence.Lob;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
